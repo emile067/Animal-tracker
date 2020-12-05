8a510e1c48bea1b89d52aebb53dd1021eae6db45
emile kamana <emileizere@gmail.com>
2020-12-06 00:05:33 +0200

    add all attributes to the animal class

--- src/main/java/Animals.java
+++ src/main/java/Animals.java
@@ -1,3 +1,6 @@
+import org.sql2o.Connection;
+
+import java.util.List;
 import java.util.Objects;
 
 public class Animals {
@@ -9,6 +12,33 @@ public class Animals {
         return name;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public String getAge() {
+        return age;
+    }
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (name) VALUES (:name)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    public static List<Animals> all() {
+        String sql = "SELECT * FROM animals";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Animals.class);
+        }
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -23,4 +53,8 @@ public class Animals {
     }
 
     private String name;
+    private int id;
+    private boolean endangered;
+    private String health = null;
+    private String age = null;
 }
--- src/test/java/AnimalsTest.java
+++ src/test/java/AnimalsTest.java
@@ -8,7 +8,7 @@ public class AnimalsTest {
 
     @Before
     public void setUp() throws Exception {
-        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "emile", "0726067969");
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "emile067", "mushimiyimana");
     }
 
 
@@ -25,10 +25,18 @@ public class AnimalsTest {
 
     @Test
     public void equals_returnsTrueIfNameIsTheSame() {
-        Animals firstPerson = new Animals("Henry");
-        Animals anotherPerson = new Animals("Henry");
-        assertTrue(firstPerson.equals(anotherPerson));
+        Animals firstAnimal = new Animals("Henry");
+        Animals anotherAnimal = new Animals("Henry");
+        assertTrue(firstAnimal.equals(anotherAnimal));
     }
+
+    @Test
+    public void save_CorrectlyIntoTheDatabase() {
+        Animals animal = new Animals("Chui");
+        animal.save();
+        assertTrue(Animals.all().get(0).equals(animal));
+    }
+
     @After
     public void tearDown() throws Exception {
         try(Connection con = DB.sql2o.open()) {
