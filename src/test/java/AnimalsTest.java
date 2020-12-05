17d69f190ff17cffdb56bd275465f1d7552db423
emile kamana <emileizere@gmail.com>
2020-12-05 17:15:21 +0200

    add DB class for connecting to the database

--- src/test/java/AnimalsTest.java
+++ src/test/java/AnimalsTest.java
@@ -1,8 +1,17 @@
-import org.junit.Test;
+import org.junit.*;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import static org.junit.Assert.*;
 
 public class AnimalsTest {
+
+    @Before
+    public void setUp() throws Exception {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "emile", "0726067969");
+    }
+
+
     @Test
     public void animal_instantiatesCorrectly_true() {
         Animals testAnimal = new Animals("Gorilla");
@@ -19,5 +28,12 @@ public class AnimalsTest {
         Animals firstPerson = new Animals("Henry");
         Animals anotherPerson = new Animals("Henry");
         assertTrue(firstPerson.equals(anotherPerson));
+    }
+    @After
+    public void tearDown() throws Exception {
+        try(Connection con = DB.sql2o.open()) {
+            String sqlAnimal = "DELETE FROM animals *;";
+            con.createQuery(sqlAnimal).executeUpdate();
+        }
     }
 }
