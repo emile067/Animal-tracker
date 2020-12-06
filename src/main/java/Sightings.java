9f4756c2a1260074a431388d639c3c02dbfc9d39
emile kamana <emileizere@gmail.com>
2020-12-06 22:45:53 +0200

    add animalId property with and save and delete functions

--- src/main/java/Sightings.java
+++ src/main/java/Sightings.java
@@ -45,7 +45,12 @@ public class Sightings implements SightingsInterface{
 
     @Override
     public void delete() {
-
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "DELETE FROM sightings WHERE id = :id;";
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
     }
 
     @Override
